3b8481edee119edc7f470ffcb537060d7bb39292
cihat <cihat02020202@gmail.com>
2022-06-24 19:40:18 +0300

    Update styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,36 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  grid-area: header;
+  padding: 8px;
+  margin-bottom: 1rem;
+  background-color: rgb(var(--color-yellow), 0.2);
+`;
+
+const Title = styled.h1`
+  font-weight: bold;
+  text-align: center;
+  font-family: Raleway, sans-serif, "Roboto";
+  color: var(--color-blue);
+  font-size: 56px;
+  -webkit-text-stroke: 2px white;
+  -webkit-background-clip: text;
+  /* -webkit-text-fill-color: transparent; */
+`;
+
 const Header = () => {
-  const HeaderContainer = styled.header`
-    text-align: center;
-    grid-area: header;
-    padding: 1rem;
-    margin-bottom: 2rem;
-    background-color: #e6c68a97;
-
-    /* &::before {
-      -webkit-backdrop-filter: blur(5px);
-      backdrop-filter: blur(5px);
-      content: "";
-    } */
-  `;
-
-  const Title = styled.h1`
-    font-weight: bold;
-    text-align: center;
-    text-align: center;
-    font-size: 56px;
-    transform: skewY(-5deg);
-    letter-spacing: 4px;
-    word-spacing: -8px;
-    color: tomato;
-    text-shadow: -1px -1px 0 firebrick, -2px -2px 0 firebrick,
-      -3px -3px 0 firebrick, -4px -4px 0 firebrick, -5px -5px 0 firebrick,
-      -6px -6px 0 firebrick, -7px -7px 0 firebrick, -8px -8px 0 firebrick,
-      -30px 20px 40px dimgrey;
-  `;
-
   return (
     <HeaderContainer>
       <Title>Todo App</Title>
--- src/containers/Todo/index.js
+++ src/containers/Todo/index.js
@@ -2,24 +2,25 @@ import TodoList from "../../components/TodoList";
 import React from "react";
 import styled from "styled-components";
 
-const TodoContainer = () => {
-  const TodoListContainer = styled.div`
-    grid-area: main;
-    display: flex;
-    margin: 0 auto;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 1rem;
-    margin-bottom: 0;
-    background-color: #fafafa;
-    border-radius: 10px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-    border: 1px solid #e6e6e6;
-    max-width: 75%;
-    width: 100%;
-  `;
+const TodoListContainer = styled.div`
+  grid-area: main;
+  display: flex;
+  margin: 0 auto;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  margin-bottom: 0;
+  background-color: #fafafa;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  border: 1px solid #e6e6e6;
+  max-width: 75%;
+  width: 100%;
+  min-height: 100%;
+`;
 
+const TodoContainer = () => {
   return (
     <TodoListContainer>
       <TodoList />
