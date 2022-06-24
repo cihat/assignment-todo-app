8447da34b102b154a6cfa7468086465690045f3b
cihat <cihat02020202@gmail.com>
2022-06-25 02:47:23 +0300

    Add using localStorage utils function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./stores/reducers";
+import { loadLocalStorage, saveLocalStorage } from "./utils/localStorage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const savedState = loadLocalStorage("todos");
+
+const store = createStore(rootReducer, savedState);
+
+store.subscribe(() => {
+  saveLocalStorage("todos", store.getState().todos);
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
