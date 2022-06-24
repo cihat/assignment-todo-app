015752a329093ecd53144874e10e476b0898d636
cihat <cihat02020202@gmail.com>
2022-06-24 16:55:07 +0300

    Add layout styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import TodoContainer from "./containers/Todo";
+import Header from "./components/Header";
+import "./styles/index.css";
 
 function App() {
   return (
     <div className="App">
-      <TodoContainer />
+      <Header />
+      <main>
+        <TodoContainer />
+      </main>
     </div>
   );
 }
