05cf1f41bbf08518548b3957721501fd5fadb274
cihat <cihat02020202@gmail.com>
2022-06-24 20:23:16 +0300

    Add dummy data and list todos

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 
-const TodoItem = () => {
-  return <div>TodoItem</div>;
+const TodoItemContainer = styled.div`
+  background-color: gray;
+`;
+
+const TodoItem = (props) => {
+  const { key, todo } = props;
+
+  return (
+    <TodoItemContainer>
+      <li>
+        <input type="radio" checked={todo.completed} />
+        <span>{todo.text}</span>
+      </li>
+    </TodoItemContainer>
+  );
 };
 
 export default TodoItem;
--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,7 +1,49 @@
 import React from "react";
+import TodoItem from "../TodoItem";
+import { v4 as uuidv4 } from "uuid";
 
 const TodoList = () => {
-  return <div>Todo List</div>;
+  const todos = [
+    {
+      id: uuidv4(),
+      text: "Take out the trash",
+      completed: true,
+    },
+    {
+      id: uuidv4(),
+      text: "Meet with the boss",
+      completed: true,
+    },
+    {
+      id: uuidv4(),
+      text: "Meet with the boss2",
+      completed: false,
+    },
+    {
+      id: uuidv4(),
+      text: "Meet with the boss3",
+      completed: false,
+    },
+    {
+      id: uuidv4(),
+      text: "Meet with the boss4",
+      completed: true,
+    },
+    {
+      id: uuidv4(),
+      text: "Meet with the boss5",
+      completed: false,
+    },
+  ];
+  return (
+    <div>
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TodoList;
