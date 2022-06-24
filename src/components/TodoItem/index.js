ab7dec37a6f7faec80643379ba85a367cfa0f1c2
cihat <cihat02020202@gmail.com>
2022-06-25 02:43:32 +0300

    Update TodoItem style and element

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -2,18 +2,38 @@ import React from "react";
 import styled from "styled-components";
 
 const TodoItemContainer = styled.div`
-  background-color: gray;
+  width: 100%;
+  overflow-y: scroll;
+`;
+
+const TodoItemStyle = styled.li`
+  list-style: square;
+  padding: 1rem;
+  margin-bottom: 0;
+  background-color: #fafafa;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  border: 1px solid #e6e6e6;
+  min-height: 100%;
+  max-width: 85%;
+  cursor: pointer;
+  &:hover {
+    background-color: #e6e6e6;
+  }
 `;
 
 const TodoItem = (props) => {
-  const { key, todo } = props;
+  const { key, todo, onClick } = props;
 
   return (
     <TodoItemContainer>
-      <li>
-        <input type="radio" checked={todo.completed} />
+      <TodoItemStyle
+        key={key}
+        onClick={onClick}
+        style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+      >
         <span>{todo.text}</span>
-      </li>
+      </TodoItemStyle>
     </TodoItemContainer>
   );
 };
