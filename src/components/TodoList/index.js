da503624cce7f9a017d025a09942dcb576750d16
cihat <cihat02020202@gmail.com>
2022-06-26 03:45:38 +0300

    Add `No todos yet` text when no todo

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,17 +1,44 @@
 import React from "react";
 import TodoItem from "../TodoItem";
+import styled from "styled-components";
+
+const ListWrapper = styled.div`
+  margin-top: 1rem;
+  max-height: 700px;
+  overflow-y: scroll;
+  min-width: 100%;
+  margin-bottom: 2rem;
+
+  p {
+    text-align: center;
+    font-size: 2.5rem;
+    font-weight: bold;
+  }
+
+  @media (min-width: 768px) {
+    max-width: 75%;
+  }
+`;
 
 const TodoList = ({ todos, toggleTodo, deleteTodo }) => {
   return (
-    <ul style={{ display: "flex", flexDirection: "column-reverse" }}>
-      {todos.map((todo) => (
-        <TodoItem
-          todo={todo}
-          addTodo={() => toggleTodo(todo.id)}
-          deleteTodo={() => deleteTodo(todo.id)}
-        />
-      ))}
-    </ul>
+    <>
+      {todos.length > 0 ? (
+        <ul style={{ display: "flex", flexDirection: "column-reverse" }}>
+          {todos.map((todo) => (
+            <TodoItem
+              todo={todo}
+              addTodo={() => toggleTodo(todo.id)}
+              deleteTodo={() => deleteTodo(todo.id)}
+            />
+          ))}
+        </ul>
+      ) : (
+        <ListWrapper>
+          <p>No todos yet!</p>
+        </ListWrapper>
+      )}
+    </>
   );
 };
 
